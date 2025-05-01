bc0048c310d6edef5d10fcbb499e27c3d2d3a413
AndranikAdyan <andranikadyan@gmail.com>
2025-05-01 21:59:12 +0400

    fix bonus part

--- philo_bonus/src/philos_bonus.c
+++ philo_bonus/src/philos_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:29:43 by aadyan            #+#    #+#             */
-/*   Updated: 2025/04/28 21:32:02 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/05/01 21:58:28 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 void	create_philos(t_table *table)
 {
 	int		index;
-	pid_t	pid;
 
 	index = 0;
 	table->start_time = get_time_in_ms();
 	while (index < table->num_of_philos)
 	{
-		pid = fork();
-		if (pid == 0)
+		table->philo[index].pid = fork();
+		if (table->philo[index].pid == 0)
 		{
-			table->philo[index].pid = pid;
 			life(&table->philo[index]);
 			pthread_join(table->philo[index].philo_death_thread, NULL);
 			exit(0);
